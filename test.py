0e32f069a4a0a41b3ba9862a86630195a748f148
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2022-12-15 18:02:29 +0400

    V1

---
+++ test.py
@@ -0,0 +1,116 @@
+# -------------------------------------------
+# char = '!~@#$%-^& *()_+/*'
+# text = input('Enter: ')
+# if text.isdigit():
+#     print('text is digit')
+# elif text.isalpha():
+#     print('text is tar')
+# elif text in char:
+#     print('text id char')
+# else:
+#     print('text is mix')
+# -------------------------------------------
+# text = 'python'
+# for i in text:
+#     print(i)
+# -------------------------------------------
+
+# number = 7854
+# for i in str(number):
+#     print(i)
+
+# -------------------------------------------
+# for i in range(0, 100, 2):
+#     print(i)
+# -------------------------------------------
+# number = int(input('Enter number: '))
+# s = ''
+# for i in str(number):
+#     s += i
+# print(s)
+# -------------------------------------------
+
+# text = input('Enter text: ')
+# for i in range(0, len(text) - 1):
+#     print(text[i], text[i + 1])
+# -------------------------------------------
+
+# >>> python - django
+#     django - python
+#     python - ml
+# print(text[0], text[1])
+# print(text[1], text[2])
+# print(text[2], text[3])
+# for i in range()
+
+# print(len(text))
+# for i in range(0, len(text)):
+#     print(text[i]) # text[0]
+    # text[1]
+    # text[2]
+    # text[3]
+# -------------------------------------------
+# for i in range(0, 6):
+#     print(i)
+# -------------------------------------------
+
+# i = 0
+# while i <= 6:
+#     print(i)
+#     i += 1
+# -------------------------------------------
+
+# i = 0
+# while True:
+#     if i <= 6:
+#         print(i)
+#         i += 1
+#     else:
+#         break
+# -------------------------------------------
+
+# import random
+
+# user_points = 0
+# pc_points = 0
+# while True:
+#     if user_points == 2:
+#         print('WIN USER')
+#         break
+#     elif pc_points == 2:
+#         print('WIN PC')
+#         break
+#     user = int(input('Enter number in range (0-5):'))
+#     pc = random.randint(0, 5)
+#     if user == pc:
+#         user_points += 1
+#         print('Duq gushakel eq')
+#     else:
+#         pc_points += 1
+#         print(f'user number = {user}\npc number = {pc}\n------CHEQ GUSHAKEL GREQ NORIC -------')
+
+# -------------------------------------------
+
+# text = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+# for i in text:
+#     if i % 2 == 0:
+#         print(i)
+#     else:
+#         continue
+
+# -------------------------------------------
+
+# text = input('Enter text: ')
+# char = '!@#$%^ .'
+# number_count = 0
+# letter_count = 0
+# char_count = 0
+# for i in text:
+#     if i.isdigit():
+#         number_count += 1
+#     elif i in char:
+#         char_count += 1
+#     else:
+#         letter_count += 1
+# print(f'{text} texi mej ka {number_count} tiv ev {letter_count} tar eb {char_count} char')
+# -------------------------------------------
